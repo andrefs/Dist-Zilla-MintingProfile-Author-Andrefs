3b33c4bf24ab166f723c17cfee9adf5fb9f63615
André Santos <andrefs@cpan.org>
2012-03-25 03:12:43 +0100

    first working version - dists created with dzil new -P Author::Andrefs have git, pass tests,   etc

--- lib/Dist/Zilla/MintingProfile/Author/Andrefs.pm
+++ lib/Dist/Zilla/MintingProfile/Author/Andrefs.pm
@@ -2,4 +2,14 @@ use strict;
 use warnings;
 package Dist::Zilla::MintingProfile::Author::Andrefs;
 
+# ABSTRACT: Andrefs's Minting Profile
+ 
+use Moose;
+use namespace::autoclean;
+with 'Dist::Zilla::Role::MintingProfile::ShareDir';
+ 
+ 
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
 1;
